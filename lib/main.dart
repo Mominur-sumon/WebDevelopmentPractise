b19c8be72d9f0e34ce7c17dba8046153d4fd1795
Md. Mominiur Rahman <mominur.sumon1@gmai;.com>
2023-09-12 00:10:51 +0600

    AppBar UI design code updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeActivity());
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.blue),
+      color: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity());
   }
 }
 
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
 
+  mySnackBar(message, context){
+
+    return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text(message))
+   );
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: Text('My App'),),
-      body: Text('Hello Guys'),
+      appBar: AppBar(
+        title: Text('My App'),
+        titleSpacing: 50,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 150,
+        backgroundColor: Colors.amber,
+        actions: [
+
+          IconButton(onPressed: (){mySnackBar("Menu bar will update shortly", context);}, icon: Icon(Icons.menu)),
+          IconButton(onPressed: (){mySnackBar("Search function will updtae", context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){mySnackBar("seting function will updtae ", context);}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){mySnackBar("Comments", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){mySnackBar("mail function will update", context);}, icon: Icon(Icons.email)),
+
+        ],
+
+      ),
+     
+      //left navigation drawer
     );
   }
 }
